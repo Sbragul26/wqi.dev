075b17596659aaba01f55aa7910948fff799edff
Sbragul26 <sbragul26@gmail.com>
2025-02-28 19:22:19 +0600

    Added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './pages/home'
+import Welcome from './pages/getstarted'
+import Prices from './pages/prices'
+import News from './pages/news'
+const App = () => {
+  return (
+    <div>
+      <News />
+    </div>
+  )
+}
+export default App
